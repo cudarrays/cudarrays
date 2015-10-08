50383c03102c179c02fcfe0143d70bba6376e212
Javier Cabezas <javier.cabezas@gmail.com>
2015-10-08 15:39:54 +0200

    Move to CUDA 7.5 in travis-ci.

--- scripts/travis/install_build_env.sh
+++ scripts/travis/install_build_env.sh
@@ -14,7 +14,7 @@ apt-get install \
     gcc-4.9 g++-4.9
 
 # Install CUDA
-CUDA_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_7.0-28_amd64.deb
+CUDA_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
 CUDA_FILE=/tmp/cuda_install.deb
 curl $CUDA_URL -o $CUDA_FILE
 dpkg -i $CUDA_FILE
@@ -22,8 +22,8 @@ rm -f $CUDA_FILE
 apt-get -y update
 # Install the minimal CUDA subpackages required to test Caffe build.
 # For a full CUDA installation, add 'cuda' to the list of packages.
-apt-get -y install cuda-core-7-0 cuda-cudart-7-0 cuda-cudart-dev-7-0
+apt-get -y install cuda-core-7-5 cuda-cudart-7-5 cuda-cudart-dev-7-5
 # Create CUDA symlink at /usr/local/cuda
 # (This would normally be created by the CUDA installer, but we create it
 # manually since we did a partial installation.)
-ln -s /usr/local/cuda-7.0 /usr/local/cuda
+ln -s /usr/local/cuda-7.5 /usr/local/cuda
