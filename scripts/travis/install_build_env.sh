f24c601778200dcfdf28eb92dff8531aac698e9a
Javier Cabezas <javier.cabezas@gmail.com>
2015-11-10 06:51:57 +0100

    Use wget instead of curl

--- scripts/travis/install_build_env.sh
+++ scripts/travis/install_build_env.sh
@@ -2,13 +2,12 @@
 # Script called by Travis to install the build environment for CUDArrays. This script must be called with sudo.
 
 set -e
-set -x
 
 # Install CUDA
 echo "Installing CUDA"
 CUDA_URL=http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run
 CUDA_FILE=/tmp/cuda_install.run
-curl $CUDA_URL -o $CUDA_FILE
+wget --show-progress $CUDA_URL -O $CUDA_FILE
 chmod a+x $CUDA_FILE
 $CUDA_FILE --toolkit --toolkitpath=$HOME/cuda -silent
 rm -f $CUDA_FILE
@@ -17,6 +16,6 @@ rm -f $CUDA_FILE
 echo "Installing CMake"
 CMAKE_URL=http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz
 CMAKE_FILE=/tmp/cmake.tar.gz
-curl $CMAKE_URL -o $CMAKE_FILE
+wget --show-progress $CMAKE_URL -O $CMAKE_FILE
 tar -xvf $CMAKE_FILE -C $HOME
 rm -f $CMAKE_FILE
