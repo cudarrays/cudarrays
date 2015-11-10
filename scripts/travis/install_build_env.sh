f1bbb3e956149b96e3bbf4c9b0cb20b69c487599
Javier Cabezas <javier.cabezas@gmail.com>
2015-11-10 06:41:20 +0100

    Add progress info in travis scripts

--- scripts/travis/install_build_env.sh
+++ scripts/travis/install_build_env.sh
@@ -2,8 +2,10 @@
 # Script called by Travis to install the build environment for CUDArrays. This script must be called with sudo.
 
 set -e
+set -x
 
 # Install CUDA
+echo "Installing CUDA"
 CUDA_URL=http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run
 CUDA_FILE=/tmp/cuda_install.run
 curl $CUDA_URL -o $CUDA_FILE
@@ -12,6 +14,7 @@ $CUDA_FILE --toolkit --toolkitpath=$HOME/cuda -silent
 rm -f $CUDA_FILE
 
 # Install CMake
+echo "Installing CMake"
 CMAKE_URL=http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz
 CMAKE_FILE=/tmp/cmake.tar.gz
 curl $CMAKE_URL -o $CMAKE_FILE
